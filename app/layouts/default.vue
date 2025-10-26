dfd8967f6e6f65e3020734f848961a4c9acb7599
Bahana Alana Khansa <alanabahana102@gmail.com>
2025-10-27 00:52:04 +0700

    git init

---
+++ app/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <UHeader title="Quick Games">
+
+    </UHeader>
+
+    <UMain class="mt-3">
+      <UContainer>
+        <slot />
+      </UContainer>
+    </UMain>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <ol class="list-disc">
+    <li> <NuxtLink to="/multiply">Multiply</NuxtLink> </li>
+    <li> <NuxtLink to="/multiply">Add</NuxtLink> </li>
+    <li> <NuxtLink to="/multiply">Modulo</NuxtLink> </li>
+  </ol>
+</template>
